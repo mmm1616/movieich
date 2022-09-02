4a966855ea2934a334f25c16d48ca9473ae598b2
MikiAsai <106724109+mmm1616@users.noreply.github.com>
2022-09-02 11:39:41 +0000

    [管理者]ログイン・会員一覧

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,15 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 
   # GET /resource/sign_in
   # def new
@@ -18,10 +27,14 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,14 @@
 class Admin::UsersController < ApplicationController
+     
+  def index
+    # @users = User.page(params[:page]).per(10)
+    @users = User.find(params[:id])
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:user_name,:introduction,:is_deleted)
+  end
 end
