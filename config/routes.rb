6f4a27fe44bdeebf0b0acb56bca9b9aa5b35fde1
MikiAsai <106724109+mmm1616@users.noreply.github.com>
2022-09-02 07:45:17 +0000

    Initial Commit1

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   sessions: 'public/sessions',
   passwords: 'public/passwords'
 }
+　devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope module: 'public' do
    root to: "homes#top"
