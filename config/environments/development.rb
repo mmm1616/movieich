6353c66a709911e47812444c195b32cad55e9ca7
MikiAsai <106724109+mmm1616@users.noreply.github.com>
2022-09-02 08:07:05 +0000

    view追加・ActiveStorageinstall

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "dd4d8d3aa9974674ad877a47c281b3ed.vfs.cloud9.us-east-1.amazonaws.com"
   
+  config.active_job.queue_adapter = :inline
+  
 end
