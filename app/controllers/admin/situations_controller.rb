a63e43736e5b3398899e3c5e4edcaffc2c414413
MikiAsai <106724109+mmm1616@users.noreply.github.com>
2022-09-05 01:15:11 +0000

    [管理者]シチュエーション一覧・追加・編集

--- app/controllers/admin/situations_controller.rb
+++ app/controllers/admin/situations_controller.rb
@@ -1,15 +1,35 @@
 class Admin::SituationsController < ApplicationController
     
  def index
+  @situations = Situation.all
+  @situation = Situation.new
  end
  
  def create
+  @situation = Situation.new(situation_params)
+  @situation.save
+  redirect_to admin_situations_path
  end
  
  def edit
+  @situation = Situation.find(params[:id])
  end
 
  def update
+  @situation = Situation.find(params[:id])
+    
+    if  @situation.update(situation_params)
+        redirect_to admin_situations_path
+        flash[:notice] = "シチュエーションが更新されました。"
+    else
+        render :edit
+    end
+ end
+ 
+ private
+  
+ def genre_params
+    params.require(:situation).permit(:name)
  end
  
 end
