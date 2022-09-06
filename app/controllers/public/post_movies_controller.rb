9dc4f6c3f43681157ed6ae4b81417c144a82818d
MikiAsai <106724109+mmm1616@users.noreply.github.com>
2022-09-06 02:16:49 +0000

    [ユーザー]投稿一覧

--- app/controllers/public/post_movies_controller.rb
+++ app/controllers/public/post_movies_controller.rb
@@ -1,2 +1,15 @@
 class Public::PostMoviesController < ApplicationController
+   def index
+   
+   @post_movies = PostMovie.all
+
+    if params[:situation_id] == nil
+    @post_movies = PostMovie.page(params[:page]).per(10)
+    else
+    post_movies = PostMovie.where(situation_id: params[:situation_id])
+    @post_movies = post_movies.page(params[:page]).per(10)
+    @situation = Situation.find(params[:situation_id])
+    end
+    
+   end
 end
